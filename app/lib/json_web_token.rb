282abd0d8481ab0daa51cfc1aed50841dcd2b482
dorlandimort <dorlandimort@gmail.com>
2017-12-04 15:39:57 -0600

    Authorization tests added

---
+++ app/lib/json_web_token.rb
@@ -0,0 +1,21 @@
+class JsonWebToken
+  # secret to encode and decode token
+  HMAC_SECRET = Rails.application.secrets.secret_key_base
+
+  def self.encode(payload, exp = 24.hours.from_now)
+    # set expire time
+    payload[:exp] = exp.to_i
+    # sign token with application secret
+    JWT.encode(payload, HMAC_SECRET)
+  end
+
+  def self.decode(token)
+    # get payload; first inex in decoded array
+    body = JWT.decode(token, HMAC_SECRET)[0]
+    HashWithIndifferentAccess.new body
+    # rescue from expeiry exception
+  rescue JWT::ExpiredSignature, JWT::VerificationError => e
+      # raise custom error
+      raise ExceptionHandler::InvalidToken, e.message
+  end
+end
